6b0472fd289ae1ee47394f2f4ade88a4c4c6bb56
Francisco José Ferrer Rodríguez <franferrerrodriguez@gmail.com>
2022-05-31 15:52:18 +0200

    config bodyParser

--- 01-server/index.ts
+++ 01-server/index.ts
@@ -1,7 +1,11 @@
 import Server from "./class/server";
 import router from "./routes/router";
+import bodyParser from 'body-parser';
 
 const server = new Server();
+
+server.app.use(bodyParser.urlencoded({ extended: true }));
+server.app.use(bodyParser.json());
 
 server.app.use('/', router);
 
